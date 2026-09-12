e2ac8da38d9efce30b35bce2963c80a271fc1931
Brandon Eastwell <92370206+BrandonEastwell@users.noreply.github.com>
2026-09-12 19:50:00 +0100

    refactor: top menu drop down list

--- frontend/components/BarItem.tsx
+++ frontend/components/BarItem.tsx
@@ -5,9 +5,6 @@ import {AnimatePresence, motion} from "motion/react";
 
 interface SidebarItemProps {
     svgPaths: string[];
-    dropDown?: boolean;
-    dropDownItems?: { text: string, onClick: () => void | Promise<void> }[];
-    dropDownOpen?: boolean;
     label: string;
     onClick?: () => void;
     iconSize?: number;
@@ -20,9 +17,6 @@ export default function BarItem({
     svgPaths,
     label,
     onClick,
-    dropDown,
-    dropDownItems,
-    dropDownOpen,
     fill = "black",
     iconSize = 30,
     strokeWidth = 2,
@@ -31,20 +25,6 @@ export default function BarItem({
     return (
         <motion.div aria-label={label} className={className} onClick={onClick} animate={{}}>
             <DrawIcon fill={fill} svgPaths={svgPaths} strokeWidth={strokeWidth} iconSize={iconSize} />
-            <AnimatePresence>
-                {dropDown && dropDownOpen && dropDownItems &&
-                        <motion.div
-                            initial={{ opacity: 0, top: 50 }}
-                            animate={{ opacity: 1, top: 60 }}
-                            exit={{ opacity: 0, top: 50 }}
-                            className="dropdown absolute z-50 min-w-28 overflow-hidden text-nowrap rounded-xl text-sm font-mono border border-white/40 bg-rose/35 p-1 shadow-[0_8px_32px_rgba(73,88,103,0.18)] backdrop-blur-xl backdrop-saturate-150 ring-1 ring-rose/10"
-                        >
-                            { dropDownItems.map((item, index) => (
-                                <div className="dropdown-item rounded-lg p-1 cursor-pointer hover:bg-white/30 hover:shadow-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-rose/30" onClick={item.onClick} key={index + item.text}>{item.text}</div>
-                            )) }
-                        </motion.div>
-                }
-            </AnimatePresence>
         </motion.div>
     );
 }
