7318cfbff9162df9fd49bd33c0fb7deb903e2f06
Brandon Eastwell <92370206+BrandonEastwell@users.noreply.github.com>
2026-09-16 16:30:32 +0100

    feat: port changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   app.enableCors({ origin: process.env.ORIGIN, credentials: true });
   app.use(cookieParser());
   app.useGlobalPipes(new ZodValidationPipe());
-  const port = process.env.PORT ?? 8000;
+  const port = process.env.PORT ?? 3001;
   await app.listen(port, '0.0.0.0');
 }
 void bootstrap();
